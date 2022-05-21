e2d76913203e5fa4072048ef56f9304d37f6211b
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2022-05-20 22:51:59 -0700

    add metric extraction behaviors
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

---
+++ src/main/kotlin/com/sortinghat/metrics_extractor/domain/behaviors/ExtractionResult.kt
@@ -0,0 +1,3 @@
+package com.sortinghat.metrics_extractor.domain.behaviors
+
+interface ExtractionResult
---
+++ src/main/kotlin/com/sortinghat/metrics_extractor/domain/behaviors/MetricExtractor.kt
@@ -0,0 +1,7 @@
+package com.sortinghat.metrics_extractor.domain.behaviors
+
+import com.sortinghat.metrics_extractor.domain.model.System
+
+interface MetricExtractor {
+    fun execute(system: System): ExtractionResult
+}
