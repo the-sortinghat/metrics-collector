1f16d6e43dff591f6c6f8388ff58c62915925fd4
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2022-05-23 15:54:01 -0700

    extract data sources per component metric
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

---
+++ src/main/kotlin/com/sortinghat/metrics_extractor/domain/services/DataSourcesPerComponentMetric.kt
@@ -0,0 +1,28 @@
+package com.sortinghat.metrics_extractor.domain.services
+
+import com.sortinghat.metrics_extractor.domain.behaviors.ExtractionResult
+import com.sortinghat.metrics_extractor.domain.behaviors.MetricExtractor
+import com.sortinghat.metrics_extractor.domain.behaviors.PerComponentMetric
+import com.sortinghat.metrics_extractor.domain.model.System
+
+/**
+ * Number of clients that consume messages published by a given component
+ * Dimension: Async Coupling
+ */
+class DataSourcesPerComponentMetric: MetricExtractor {
+    override fun execute(system: System): ExtractionResult {
+        val forModules = system.usages
+            .groupBy { it.service.module }
+            .mapValues { it.value.distinctBy { usage -> usage.database to usage.namespace } }
+            .mapValues { it.value.size }
+
+        val forServices = system.usages
+            .groupBy { it.service }
+            .mapValues { it.value.size }
+
+        return PerComponentMetric(
+            modules = system.modules.associateWith { forModules[it] ?: 0 },
+            services = system.services.associateWith { forServices[it] ?: 0 }
+        )
+    }
+}
---
+++ src/test/kotlin/com/sortinghat/metrics_extractor/domain/services/DataSourcesPerComponentMetricTest.kt
@@ -0,0 +1,142 @@
+package com.sortinghat.metrics_extractor.domain.services
+
+import com.sortinghat.metrics_extractor.domain.behaviors.PerComponentMetric
+import com.sortinghat.metrics_extractor.domain.model.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class DataSourcesPerComponentMetricTest {
+
+    private fun createSystem(): System {
+        val system = System(id = "1", name = "InterSCity", "")
+        system.addUsage(
+            DatabaseUsage(
+                service = Service(
+                    id = "1",
+                    name = "Resource Adaptor",
+                    responsibility = "",
+                    operations = listOf(),
+                    module = Module(id = "1", "Resource Adaptor")
+                ),
+                database = Database.create("1", "MySQL"),
+                role = "principal",
+                namespace = "Resource Adaptor DB",
+                accessType = DatabaseAccessType.ReadWrite
+            )
+        )
+        system.addUsage(
+            DatabaseUsage(
+                service = Service(
+                    id = "2",
+                    name = "Resource Catalogue",
+                    responsibility = "",
+                    operations = listOf(),
+                    module = Module(id = "2", "Resource Catalog")
+                ),
+                database = Database.create("2", "MySQL"),
+                role = "principal",
+                namespace = "Resource Catalogue DB",
+                accessType = DatabaseAccessType.ReadWrite
+            )
+        )
+        system.addUsage(
+            DatabaseUsage(
+                service = Service(
+                    id = "3",
+                    name = "Data Collector",
+                    responsibility = "",
+                    operations = listOf(),
+                    module = Module(id = "3", "Data Collector")
+                ),
+                database = Database.create("3", "MySQL"),
+                role = "principal",
+                namespace = "Data Collector DB",
+                accessType = DatabaseAccessType.ReadWrite
+            )
+        )
+
+        return system
+    }
+
+    @Test
+    fun `should return the number of data sources each service accesses`() {
+        val system = createSystem()
+        val services = system.services.toList()
+        val expected = mapOf(
+            services[0] to 1,
+            services[1] to 1,
+            services[2] to 1,
+        )
+
+        val metricExtractor = DataSourcesPerComponentMetric()
+        val actual = (metricExtractor.execute(system) as PerComponentMetric).services
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `should return the number of data source each module accesses`() {
+        val system = createSystem()
+
+        system.addUsage(
+            DatabaseUsage(
+                service = Service(
+                    id = "4",
+                    name = "Data Collector Outro",
+                    responsibility = "",
+                    operations = listOf(),
+                    module = Module(id = "3", "Data Collector")
+                ),
+                database = Database.create("4", "MySQL"),
+                role = "cache",
+                namespace = "Data Collector cache",
+                accessType = DatabaseAccessType.ReadWrite
+            )
+        )
+
+        val modules = system.modules.toList()
+        val expected = mapOf(
+            modules[0] to 1,
+            modules[1] to 1,
+            modules[2] to 2,
+        )
+
+        val metricExtractor = DataSourcesPerComponentMetric()
+        val actual = (metricExtractor.execute(system) as PerComponentMetric).modules
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `should not count services that uses the same database in a module`() {
+        val system = createSystem()
+
+        system.addUsage(
+            DatabaseUsage(
+                service = Service(
+                    id = "4",
+                    name = "Data Collector Outro",
+                    responsibility = "",
+                    operations = listOf(),
+                    module = Module(id = "3", "Data Collector")
+                ),
+                database = Database.create("3", "MySQL"),
+                role = "principal",
+                namespace = "Data Collector DB",
+                accessType = DatabaseAccessType.ReadWrite
+            )
+        )
+
+        val modules = system.modules.toList()
+        val expected = mapOf(
+            modules[0] to 1,
+            modules[1] to 1,
+            modules[2] to 1,
+        )
+
+        val metricExtractor = DataSourcesPerComponentMetric()
+        val actual = (metricExtractor.execute(system) as PerComponentMetric).modules
+
+        assertEquals(expected, actual)
+    }
+}
