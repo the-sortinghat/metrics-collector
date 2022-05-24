b9e4183e2df42aa56e66ff2b1dc1e3ce83d6e222
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2022-05-23 17:50:22 -0700

    extract shared databases between components metric
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

---
+++ src/main/kotlin/com/sortinghat/metrics_extractor/domain/services/SharedDatabasesMetric.kt
@@ -0,0 +1,41 @@
+package com.sortinghat.metrics_extractor.domain.services
+
+import com.sortinghat.metrics_extractor.domain.behaviors.ExtractionResult
+import com.sortinghat.metrics_extractor.domain.behaviors.MetricExtractor
+import com.sortinghat.metrics_extractor.domain.behaviors.PerComponentResult
+import com.sortinghat.metrics_extractor.domain.model.System
+
+/**
+ * Number of data sources that each component share with others
+ * Dimension: Data Coupling
+ */
+class SharedDatabasesMetric: MetricExtractor {
+    override fun execute(system: System): ExtractionResult {
+        val modules = system.modules.associateWith { 0 }.toMutableMap()
+        val services = system.services.associateWith { 0 }.toMutableMap()
+
+        system.usages
+            .groupBy { it.database to it.namespace }
+            .mapValues { it.value.distinctBy { usage -> usage.service.module } }
+            .values
+            .forEach { sharedDatabases ->
+                sharedDatabases.forEach { usage ->
+                    modules[usage.service.module] = sharedDatabases.size - 1
+                }
+            }
+
+        system.usages
+            .groupBy { it.database to it.namespace }
+            .values
+            .forEach { sharedDatabases ->
+                sharedDatabases.forEach { usage ->
+                    services[usage.service] = sharedDatabases.size - 1
+                }
+            }
+
+        return PerComponentResult(
+            modules = modules,
+            services = services
+        )
+    }
+}
---
+++ src/test/kotlin/com/sortinghat/metrics_extractor/domain/services/SharedDatabasesMetricTest.kt
@@ -0,0 +1,120 @@
+package com.sortinghat.metrics_extractor.domain.services
+
+import com.sortinghat.metrics_extractor.domain.behaviors.PerComponentResult
+import com.sortinghat.metrics_extractor.domain.model.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class SharedDatabasesMetricTest {
+    private fun createSystem(): System {
+        val system = System(id = "1", name = "InterSCity", "")
+        system.addUsage(
+            DatabaseUsage(
+                service = Service(
+                    id = "1",
+                    name = "Resource Adaptor",
+                    responsibility = "",
+                    operations = listOf(),
+                    module = Module(id = "1", "Resource Adaptor")
+                ),
+                database = Database.create("1", "MySQL"),
+                role = "principal",
+                namespace = "Resource Adaptor DB",
+                accessType = DatabaseAccessType.ReadWrite
+            )
+        )
+        system.addUsage(
+            DatabaseUsage(
+                service = Service(
+                    id = "2",
+                    name = "Resource Catalogue",
+                    responsibility = "",
+                    operations = listOf(),
+                    module = Module(id = "2", "Resource Catalog")
+                ),
+                database = Database.create("1", "MySQL"),
+                role = "principal",
+                namespace = "Resource Adaptor DB",
+                accessType = DatabaseAccessType.ReadWrite
+            )
+        )
+        system.addUsage(
+            DatabaseUsage(
+                service = Service(
+                    id = "3",
+                    name = "Data Collector",
+                    responsibility = "",
+                    operations = listOf(),
+                    module = Module(id = "3", "Data Collector")
+                ),
+                database = Database.create("3", "MySQL"),
+                role = "principal",
+                namespace = "Data Collector DB",
+                accessType = DatabaseAccessType.ReadWrite
+            )
+        )
+
+        return system
+    }
+
+    @Test
+    fun `should return the number of data sources that each service shares with the others`() {
+        val system = createSystem()
+        val services = system.services.toList()
+        val expected = mapOf(
+            services[0] to 1,
+            services[1] to 1,
+            services[2] to 0,
+        )
+        val metricExtractor = SharedDatabasesMetric()
+        val actual = (metricExtractor.execute(system) as PerComponentResult).services
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `should return the number of data sources that each module shares with the others`() {
+        val system = createSystem()
+        val modules = system.modules.toList()
+        val expected = mapOf(
+            modules[0] to 1,
+            modules[1] to 1,
+            modules[2] to 0,
+        )
+        val metricExtractor = SharedDatabasesMetric()
+        val actual = (metricExtractor.execute(system) as PerComponentResult).modules
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `should not count shared databases between services in the same module`() {
+        val system = createSystem()
+        system.addUsage(
+            DatabaseUsage(
+                service = Service(
+                    id = "4",
+                    name = "Data Collector Outro",
+                    responsibility = "",
+                    operations = listOf(),
+                    module = Module(id = "3", "Data Collector")
+                ),
+                database = Database.create("3", "MySQL"),
+                role = "principal",
+                namespace = "Data Collector DB",
+                accessType = DatabaseAccessType.ReadWrite
+            )
+        )
+
+        val modules = system.modules.toList()
+        val expected = mapOf(
+            modules[0] to 1,
+            modules[1] to 1,
+            modules[2] to 0,
+        )
+        val metricExtractor = SharedDatabasesMetric()
+        val actual = (metricExtractor.execute(system) as PerComponentResult).modules
+
+        assertEquals(expected, actual)
+    }
+}
